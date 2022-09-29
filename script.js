b5a349997c36ddcc4ee77818dbf57e4c4dbc40e7
Adam <adam_stawiarski7@wp.pl>
2022-09-29 14:39:19 +0200

    created js email validator

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 const input = document.querySelector(`input`)
 const btn = document.querySelector(`button`)
+const emailInfo = document.querySelector(`.header__input-validation`)
 
+const correctEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+
+const checkEmail = () => {
+
+    if(input.value.match(correctEmail)){
+        emailInfo.textContent = `Succes`
+        emailInfo.classList.add(`color-succes`)
+        input.value.textContent = ``
+        input.value = ``
+    } else {
+        emailInfo.textContent = `Please provide a valid email`
+        emailInfo.classList.remove(`color-succes`)
+        input.value = ``
+    }
+}
+
+const enterKeyup = (e) => {
+    if(e.key === `Enter`) {
+        checkEmail()
+    }
+}
+
+
+btn.addEventListener(`click`, checkEmail)
+input.addEventListener(`keyup`, enterKeyup)
